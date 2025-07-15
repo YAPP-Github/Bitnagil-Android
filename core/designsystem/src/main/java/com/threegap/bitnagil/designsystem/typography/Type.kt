dda6186c0131b40224cb1a2464ff2e37333846f6
yunsehwan <sh10302@naver.com>
2025-07-15 20:28:02 +0900

    FIX: textStyle사용시 반복적으로 copy가 호출되어 매번 새 객체가 생성될 수 있는 부분 수정

--- core/designsystem/src/main/java/com/threegap/bitnagil/designsystem/typography/Type.kt
+++ core/designsystem/src/main/java/com/threegap/bitnagil/designsystem/typography/Type.kt
@@ -94,39 +94,63 @@ class BitnagilTypography internal constructor(
         lineHeight = 20,
     ),
 ) {
-    val headline1Bold: TextStyle @Composable get() = _headline1.copy(fontWeight = FontWeight.Bold).toDpTextStyle
-    val headline1Medium: TextStyle @Composable get() = _headline1.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    private val _headline1Bold = _headline1.copy(fontWeight = FontWeight.Bold)
+    private val _headline1Medium = _headline1.copy(fontWeight = FontWeight.Medium)
+    private val _headline2Bold = _headline2.copy(fontWeight = FontWeight.Bold)
+    private val _headline2Medium = _headline2.copy(fontWeight = FontWeight.Medium)
+    private val _title1Bold = _title1.copy(fontWeight = FontWeight.Bold)
+    private val _title1SemiBold = _title1.copy(fontWeight = FontWeight.SemiBold)
+    private val _title1Medium = _title1.copy(fontWeight = FontWeight.Medium)
+    private val _title2Bold = _title2.copy(fontWeight = FontWeight.Bold)
+    private val _title2Medium = _title2.copy(fontWeight = FontWeight.Medium)
+    private val _title3SemiBold = _title3.copy(fontWeight = FontWeight.SemiBold)
+    private val _title3Medium = _title3.copy(fontWeight = FontWeight.Medium)
+    private val _subtitle1SemiBold = _subtitle1.copy(fontWeight = FontWeight.SemiBold)
+    private val _subtitle1Medium = _subtitle1.copy(fontWeight = FontWeight.Medium)
+    private val _body1SemiBold = _body1.copy(fontWeight = FontWeight.SemiBold)
+    private val _body1Medium = _body1.copy(fontWeight = FontWeight.Medium)
+    private val _body2SemiBold = _body2.copy(fontWeight = FontWeight.SemiBold)
+    private val _body2Medium = _body2.copy(fontWeight = FontWeight.Medium)
+    private val _caption1SemiBold = _caption1.copy(fontWeight = FontWeight.SemiBold)
+    private val _caption1Medium = _caption1.copy(fontWeight = FontWeight.Medium)
+    private val _caption1UnderlineSemiBold = _caption1Underline.copy(fontWeight = FontWeight.SemiBold)
+    private val _caption1UnderlineMedium = _caption1Underline.copy(fontWeight = FontWeight.Medium)
+    private val _caption2SemiBold = _caption2.copy(fontWeight = FontWeight.SemiBold)
+    private val _caption2Medium = _caption2.copy(fontWeight = FontWeight.Medium)
+
+    val headline1Bold: TextStyle @Composable get() = _headline1Bold.toDpTextStyle
+    val headline1Medium: TextStyle @Composable get() = _headline1Medium.toDpTextStyle
     val headline1Regular: TextStyle @Composable get() = _headline1.toDpTextStyle
-    val headline2Bold: TextStyle @Composable get() = _headline2.copy(fontWeight = FontWeight.Bold).toDpTextStyle
-    val headline2Medium: TextStyle @Composable get() = _headline2.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val headline2Bold: TextStyle @Composable get() = _headline2Bold.toDpTextStyle
+    val headline2Medium: TextStyle @Composable get() = _headline2Medium.toDpTextStyle
     val headline2Regular: TextStyle @Composable get() = _headline2.toDpTextStyle
-    val title1Bold: TextStyle @Composable get() = _title1.copy(fontWeight = FontWeight.Bold).toDpTextStyle
-    val title1SemiBold: TextStyle @Composable get() = _title1.copy(fontWeight = FontWeight.SemiBold).toDpTextStyle
-    val title1Medium: TextStyle @Composable get() = _title1.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val title1Bold: TextStyle @Composable get() = _title1Bold.toDpTextStyle
+    val title1SemiBold: TextStyle @Composable get() = _title1SemiBold.toDpTextStyle
+    val title1Medium: TextStyle @Composable get() = _title1Medium.toDpTextStyle
     val title1Regular: TextStyle @Composable get() = _title1.toDpTextStyle
-    val title2Bold: TextStyle @Composable get() = _title2.copy(fontWeight = FontWeight.Bold).toDpTextStyle
-    val title2Medium: TextStyle @Composable get() = _title2.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val title2Bold: TextStyle @Composable get() = _title2Bold.toDpTextStyle
+    val title2Medium: TextStyle @Composable get() = _title2Medium.toDpTextStyle
     val title2Regular: TextStyle @Composable get() = _title2.toDpTextStyle
-    val title3SemiBold: TextStyle @Composable get() = _title3.copy(fontWeight = FontWeight.SemiBold).toDpTextStyle
-    val title3Medium: TextStyle @Composable get() = _title3.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val title3SemiBold: TextStyle @Composable get() = _title3SemiBold.toDpTextStyle
+    val title3Medium: TextStyle @Composable get() = _title3Medium.toDpTextStyle
     val title3Regular: TextStyle @Composable get() = _title3.toDpTextStyle
-    val subtitle1SemiBold: TextStyle @Composable get() = _subtitle1.copy(fontWeight = FontWeight.SemiBold).toDpTextStyle
-    val subtitle1Medium: TextStyle @Composable get() = _subtitle1.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val subtitle1SemiBold: TextStyle @Composable get() = _subtitle1SemiBold.toDpTextStyle
+    val subtitle1Medium: TextStyle @Composable get() = _subtitle1Medium.toDpTextStyle
     val subtitle1Regular: TextStyle @Composable get() = _subtitle1.toDpTextStyle
-    val body1SemiBold: TextStyle @Composable get() = _body1.copy(fontWeight = FontWeight.SemiBold).toDpTextStyle
-    val body1Medium: TextStyle @Composable get() = _body1.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val body1SemiBold: TextStyle @Composable get() = _body1SemiBold.toDpTextStyle
+    val body1Medium: TextStyle @Composable get() = _body1Medium.toDpTextStyle
     val body1Regular: TextStyle @Composable get() = _body1.toDpTextStyle
-    val body2SemiBold: TextStyle @Composable get() = _body2.copy(fontWeight = FontWeight.SemiBold).toDpTextStyle
-    val body2Medium: TextStyle @Composable get() = _body2.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val body2SemiBold: TextStyle @Composable get() = _body2SemiBold.toDpTextStyle
+    val body2Medium: TextStyle @Composable get() = _body2Medium.toDpTextStyle
     val body2Regular: TextStyle @Composable get() = _body2.toDpTextStyle
-    val caption1SemiBold: TextStyle @Composable get() = _caption1.copy(fontWeight = FontWeight.SemiBold).toDpTextStyle
-    val caption1Medium: TextStyle @Composable get() = _caption1.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val caption1SemiBold: TextStyle @Composable get() = _caption1SemiBold.toDpTextStyle
+    val caption1Medium: TextStyle @Composable get() = _caption1Medium.toDpTextStyle
     val caption1Regular: TextStyle @Composable get() = _caption1.toDpTextStyle
-    val caption1UnderlineSemiBold: TextStyle @Composable get() = _caption1Underline.copy(fontWeight = FontWeight.SemiBold).toDpTextStyle
-    val caption1UnderlineMedium: TextStyle @Composable get() = _caption1Underline.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val caption1UnderlineSemiBold: TextStyle @Composable get() = _caption1UnderlineSemiBold.toDpTextStyle
+    val caption1UnderlineMedium: TextStyle @Composable get() = _caption1UnderlineMedium.toDpTextStyle
     val caption1UnderlineRegular: TextStyle @Composable get() = _caption1Underline.toDpTextStyle
-    val caption2SemiBold: TextStyle @Composable get() = _caption2.copy(fontWeight = FontWeight.SemiBold).toDpTextStyle
-    val caption2Medium: TextStyle @Composable get() = _caption2.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val caption2SemiBold: TextStyle @Composable get() = _caption2SemiBold.toDpTextStyle
+    val caption2Medium: TextStyle @Composable get() = _caption2Medium.toDpTextStyle
     val caption2Regular: TextStyle @Composable get() = _caption2.toDpTextStyle
     val button1: TextStyle @Composable get() = _button1.toDpTextStyle
     val button2: TextStyle @Composable get() = _button2.toDpTextStyle
