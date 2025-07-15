ff1f55aab35d9303078a993c8b54995cbb299c76
yunsehwan <sh10302@naver.com>
2025-07-15 19:44:57 +0900

    FEAT: 폰트 스타일 수정, 색상 가이드 및 테마 구현

--- core/designsystem/src/main/java/com/threegap/bitnagil/designsystem/typography/Type.kt
+++ core/designsystem/src/main/java/com/threegap/bitnagil/designsystem/typography/Type.kt
@@ -12,106 +12,74 @@ import com.threegap.bitnagil.designsystem.font.pretendard
 class BitnagilTypography internal constructor(
     private val _headline1: BitnagilTextStyle = BitnagilTextStyle(
         fontFamily = pretendard,
-        fontWeight = FontWeight.Bold,
+        fontWeight = FontWeight.Normal,
         fontSize = 26,
         lineHeight = 38,
         letterSpacing = (-0.5f),
     ),
     private val _headline2: BitnagilTextStyle = BitnagilTextStyle(
         fontFamily = pretendard,
-        fontWeight = FontWeight.Bold,
+        fontWeight = FontWeight.Normal,
         fontSize = 24,
         lineHeight = 34,
         letterSpacing = (-0.5f),
     ),
     private val _title1: BitnagilTextStyle = BitnagilTextStyle(
         fontFamily = pretendard,
-        fontWeight = FontWeight.Bold,
+        fontWeight = FontWeight.Normal,
         fontSize = 22,
         lineHeight = 32,
         letterSpacing = (-0.5f),
     ),
     private val _title2: BitnagilTextStyle = BitnagilTextStyle(
         fontFamily = pretendard,
-        fontWeight = FontWeight.Bold,
+        fontWeight = FontWeight.Normal,
         fontSize = 20,
         lineHeight = 30,
     ),
     private val _title3: BitnagilTextStyle = BitnagilTextStyle(
         fontFamily = pretendard,
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.Normal,
         fontSize = 18,
         lineHeight = 24,
     ),
-    private val _subtitle: BitnagilTextStyle = BitnagilTextStyle(
+    private val _subtitle1: BitnagilTextStyle = BitnagilTextStyle(
         fontFamily = pretendard,
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.Normal,
         fontSize = 16,
         lineHeight = 28,
     ),
-    private val _body1SemiBold: BitnagilTextStyle = BitnagilTextStyle(
-        fontFamily = pretendard,
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 16,
-        lineHeight = 24,
-    ),
-    private val _body1Medium: BitnagilTextStyle = BitnagilTextStyle(
-        fontFamily = pretendard,
-        fontWeight = FontWeight.Medium,
-        fontSize = 16,
-        lineHeight = 24,
-    ),
-    private val _body1Regular: BitnagilTextStyle = BitnagilTextStyle(
+    private val _body1: BitnagilTextStyle = BitnagilTextStyle(
         fontFamily = pretendard,
         fontWeight = FontWeight.Normal,
         fontSize = 16,
         lineHeight = 24,
     ),
-    private val _body2SemiBold: BitnagilTextStyle = BitnagilTextStyle(
+    private val _body2: BitnagilTextStyle = BitnagilTextStyle(
         fontFamily = pretendard,
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.Normal,
         fontSize = 14,
         lineHeight = 20,
         letterSpacing = (-0.5f),
-    ),
-    private val _body2Medium: BitnagilTextStyle = BitnagilTextStyle(
-        fontFamily = pretendard,
-        fontWeight = FontWeight.Medium,
-        fontSize = 14,
-        lineHeight = 20,
-        letterSpacing = (-0.5f),
-    ),
-    private val _body2Long: BitnagilTextStyle = BitnagilTextStyle(
-        fontFamily = pretendard,
-        fontWeight = FontWeight.Medium,
-        fontSize = 14,
-        lineHeight = 24,
     ),
     private val _caption1: BitnagilTextStyle = BitnagilTextStyle(
         fontFamily = pretendard,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Normal,
         fontSize = 12,
         lineHeight = 18,
     ),
     private val _caption1Underline: BitnagilTextStyle = BitnagilTextStyle(
         fontFamily = pretendard,
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 12,
-        lineHeight = 18,
-        decoration = TextDecoration.Underline,
-    ),
-    private val _caption2Underline: BitnagilTextStyle = BitnagilTextStyle(
-        fontFamily = pretendard,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Normal,
         fontSize = 12,
         lineHeight = 18,
         decoration = TextDecoration.Underline,
     ),
     private val _caption2: BitnagilTextStyle = BitnagilTextStyle(
         fontFamily = pretendard,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Normal,
         fontSize = 10,
-        lineHeight = 18,
+        lineHeight = 16,
     ),
     private val _button1: BitnagilTextStyle = BitnagilTextStyle(
         fontFamily = pretendard,
@@ -126,22 +94,40 @@ class BitnagilTypography internal constructor(
         lineHeight = 20,
     ),
 ) {
-    val headline1: TextStyle @Composable get() = _headline1.toDpTextStyle
-    val headline2: TextStyle @Composable get() = _headline2.toDpTextStyle
-    val title1: TextStyle @Composable get() = _title1.toDpTextStyle
-    val title2: TextStyle @Composable get() = _title2.toDpTextStyle
-    val title3: TextStyle @Composable get() = _title3.toDpTextStyle
-    val subtitle: TextStyle @Composable get() = _subtitle.toDpTextStyle
-    val body1SemiBold: TextStyle @Composable get() = _body1SemiBold.toDpTextStyle
-    val body1Medium: TextStyle @Composable get() = _body1Medium.toDpTextStyle
-    val body1Regular: TextStyle @Composable get() = _body1Regular.toDpTextStyle
-    val body2SemiBold: TextStyle @Composable get() = _body2SemiBold.toDpTextStyle
-    val body2Medium: TextStyle @Composable get() = _body2Medium.toDpTextStyle
-    val body2Long: TextStyle @Composable get() = _body2Long.toDpTextStyle
-    val caption1: TextStyle @Composable get() = _caption1.toDpTextStyle
-    val caption1Underline: TextStyle @Composable get() = _caption1Underline.toDpTextStyle
-    val caption2Underline: TextStyle @Composable get() = _caption2Underline.toDpTextStyle
-    val caption2: TextStyle @Composable get() = _caption2.toDpTextStyle
+    val headline1Bold: TextStyle @Composable get() = _headline1.copy(fontWeight = FontWeight.Bold).toDpTextStyle
+    val headline1Medium: TextStyle @Composable get() = _headline1.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val headline1Regular: TextStyle @Composable get() = _headline1.toDpTextStyle
+    val headline2Bold: TextStyle @Composable get() = _headline2.copy(fontWeight = FontWeight.Bold).toDpTextStyle
+    val headline2Medium: TextStyle @Composable get() = _headline2.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val headline2Regular: TextStyle @Composable get() = _headline2.toDpTextStyle
+    val title1Bold: TextStyle @Composable get() = _title1.copy(fontWeight = FontWeight.Bold).toDpTextStyle
+    val title1SemiBold: TextStyle @Composable get() = _title1.copy(fontWeight = FontWeight.SemiBold).toDpTextStyle
+    val title1Medium: TextStyle @Composable get() = _title1.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val title1Regular: TextStyle @Composable get() = _title1.toDpTextStyle
+    val title2Bold: TextStyle @Composable get() = _title2.copy(fontWeight = FontWeight.Bold).toDpTextStyle
+    val title2Medium: TextStyle @Composable get() = _title2.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val title2Regular: TextStyle @Composable get() = _title2.toDpTextStyle
+    val title3SemiBold: TextStyle @Composable get() = _title3.copy(fontWeight = FontWeight.SemiBold).toDpTextStyle
+    val title3Medium: TextStyle @Composable get() = _title3.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val title3Regular: TextStyle @Composable get() = _title3.toDpTextStyle
+    val subtitle1SemiBold: TextStyle @Composable get() = _subtitle1.copy(fontWeight = FontWeight.SemiBold).toDpTextStyle
+    val subtitle1Medium: TextStyle @Composable get() = _subtitle1.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val subtitle1Regular: TextStyle @Composable get() = _subtitle1.toDpTextStyle
+    val body1SemiBold: TextStyle @Composable get() = _body1.copy(fontWeight = FontWeight.SemiBold).toDpTextStyle
+    val body1Medium: TextStyle @Composable get() = _body1.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val body1Regular: TextStyle @Composable get() = _body1.toDpTextStyle
+    val body2SemiBold: TextStyle @Composable get() = _body2.copy(fontWeight = FontWeight.SemiBold).toDpTextStyle
+    val body2Medium: TextStyle @Composable get() = _body2.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val body2Regular: TextStyle @Composable get() = _body2.toDpTextStyle
+    val caption1SemiBold: TextStyle @Composable get() = _caption1.copy(fontWeight = FontWeight.SemiBold).toDpTextStyle
+    val caption1Medium: TextStyle @Composable get() = _caption1.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val caption1Regular: TextStyle @Composable get() = _caption1.toDpTextStyle
+    val caption1UnderlineSemiBold: TextStyle @Composable get() = _caption1Underline.copy(fontWeight = FontWeight.SemiBold).toDpTextStyle
+    val caption1UnderlineMedium: TextStyle @Composable get() = _caption1Underline.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val caption1UnderlineRegular: TextStyle @Composable get() = _caption1Underline.toDpTextStyle
+    val caption2SemiBold: TextStyle @Composable get() = _caption2.copy(fontWeight = FontWeight.SemiBold).toDpTextStyle
+    val caption2Medium: TextStyle @Composable get() = _caption2.copy(fontWeight = FontWeight.Medium).toDpTextStyle
+    val caption2Regular: TextStyle @Composable get() = _caption2.toDpTextStyle
     val button1: TextStyle @Composable get() = _button1.toDpTextStyle
     val button2: TextStyle @Composable get() = _button2.toDpTextStyle
 }
