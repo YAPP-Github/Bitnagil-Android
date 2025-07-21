0b864f6973000c2de56cd0b0579c59ac12eb7652
wjdrjs00 <kimjk3409@gmail.com>
2025-07-21 11:07:15 +0900

    Feat: RoutineSection 컴포넌트 구현
    - 개별 루틴과 해당 루틴에 포함되는 세부 루틴을 모두 보여주기 위한 RoutineSection 컴포넌트 추가

--- presentation/src/main/java/com/threegap/bitnagil/presentation/home/util/LocalDateExtension.kt
+++ presentation/src/main/java/com/threegap/bitnagil/presentation/home/util/LocalDateExtension.kt
@@ -2,12 +2,14 @@ package com.threegap.bitnagil.presentation.home.util
 
 import java.time.DayOfWeek
 import java.time.LocalDate
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.time.format.TextStyle
 import java.util.Locale
 
 private val koreanLocale = Locale.KOREAN
 private val monthYearFormatter = DateTimeFormatter.ofPattern("yyyy년 M월", koreanLocale)
+private val executionTimeFormatter = DateTimeFormatter.ofPattern("a h:mm", koreanLocale)
 
 fun LocalDate.getCurrentWeekDays(): List<LocalDate> =
     (0..6).map { this.with(DayOfWeek.MONDAY).plusDays(it.toLong()) }
@@ -20,3 +22,11 @@ fun LocalDate.formatDayOfWeekShort(): String =
 
 fun LocalDate.formatDayOfMonth(): String =
     this.dayOfMonth.toString()
+
+fun String.formatExecutionTime(): String =
+    try {
+        val formattedTime = LocalTime.parse(this).format(executionTimeFormatter)
+        "$formattedTime 부터 시작"
+    } catch (e: Exception) {
+        "시간 미정"
+    }
