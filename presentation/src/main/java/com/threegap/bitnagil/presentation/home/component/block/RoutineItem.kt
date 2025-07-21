9cf0dbfd4b96ca0269813498c36775f99a6bf287
wjdrjs00 <kimjk3409@gmail.com>
2025-07-21 10:45:19 +0900

    Feat: RoutineItem 컴포넌트 구현
    - 개별 루틴을 보여주기 위한 RoutineItem 컴포넌트 추가

---
+++ presentation/src/main/java/com/threegap/bitnagil/presentation/home/component/block/RoutineItem.kt
@@ -0,0 +1,104 @@
+package com.threegap.bitnagil.presentation.home.component.block
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.threegap.bitnagil.designsystem.BitnagilTheme
+import com.threegap.bitnagil.presentation.home.model.RoutineUiModel
+import com.threegap.bitnagil.presentation.home.model.SubRoutineUiModel
+
+@Composable
+fun RoutineItem(
+    routine: RoutineUiModel,
+    onRoutineToggle: (Boolean) -> Unit,
+    onMoreClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            // todo: 리플효과 제거하기
+            .clickable(
+                onClick = { onRoutineToggle(!routine.isCompleted) },
+            )
+            .height(61.dp)
+            .background(
+                color = BitnagilTheme.colors.lightBlue75,
+                shape = RoundedCornerShape(8.dp),
+            )
+            .padding(
+                start = 16.dp,
+            ),
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            // todo: 아이콘 변경하기
+            Icon(
+                imageVector = Icons.Default.Check,
+                contentDescription = null,
+                tint = if (routine.isCompleted) Color.Black else Color.Gray,
+            )
+
+            Text(
+                text = routine.routineName,
+                style = BitnagilTheme.typography.subtitle1SemiBold,
+                color = BitnagilTheme.colors.navy500,
+            )
+        }
+
+        // todo: 아이콘 변경하기
+        Icon(
+            imageVector = Icons.Default.MoreVert,
+            contentDescription = null,
+            modifier = Modifier
+                // todo: 리플효과 제거하기
+                .clickable {
+                    onMoreClick()
+                }
+                .padding(12.dp)
+                .size(24.dp),
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun RoutineItemPreview() {
+    RoutineItem(
+        routine = RoutineUiModel(
+            routineId = "uuid1",
+            routineName = "개운하게 일어나기",
+            executionTime = "20:30:00",
+            isCompleted = false,
+            isModified = false,
+            subRoutines = listOf(
+                SubRoutineUiModel("uuid1", "물 마시기",1),
+                SubRoutineUiModel("uuid2", "스트레칭하기",2),
+                SubRoutineUiModel("uuid3", "심호흡하기",3),
+            ),
+        ),
+        onRoutineToggle = { },
+        onMoreClick = { },
+    )
+}
