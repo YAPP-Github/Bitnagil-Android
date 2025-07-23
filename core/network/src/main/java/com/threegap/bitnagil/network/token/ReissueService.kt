99fb5d97f7deffae0f405fd0f5091a960b4c3882
wjdrjs00 <kimjk3409@gmail.com>
2025-07-23 11:53:54 +0900

    Feat: 토큰 재발급 api 서비스 추가
    - AuthToken 데이터 클래스 추가

---
+++ core/network/src/main/java/com/threegap/bitnagil/network/token/ReissueService.kt
@@ -0,0 +1,13 @@
+package com.threegap.bitnagil.network.token
+
+import com.threegap.bitnagil.network.model.AuthToken
+import com.threegap.bitnagil.network.model.BaseResponse
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface ReissueService {
+    @POST("/api/v1/auth/token/reissue")
+    suspend fun reissueToken(
+        @Header("Refresh-Token") refreshToken: String
+    ): BaseResponse<AuthToken>
+}
