f2900eb211f1643cf123d33d58078cc49dead7b5
wjdrjs00 <kimjk3409@gmail.com>
2025-07-29 13:01:24 +0900

    Feat: BitnagilCheckBox 컴포저블 기능 추가
    - onCheckedChange 콜백 추가하여 외부에서 체크 상태를 변경할 수 있도록 함

--- core/designsystem/src/main/java/com/threegap/bitnagil/designsystem/component/atom/BitnagilCheckBox.kt
+++ core/designsystem/src/main/java/com/threegap/bitnagil/designsystem/component/atom/BitnagilCheckBox.kt
@@ -6,6 +6,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,17 +20,26 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.threegap.bitnagil.designsystem.BitnagilTheme
 import com.threegap.bitnagil.designsystem.R
+import com.threegap.bitnagil.designsystem.modifier.clickableWithoutRipple
 
 @Composable
 fun BitnagilCheckBox(
     checked: Boolean,
     modifier: Modifier = Modifier,
+    onCheckedChange: ((Boolean) -> Unit)? = null,
     colors: BitnagilCheckBoxColor = BitnagilCheckBoxColor.terms(),
 ) {
     val iconColor = if (checked) colors.checkedColor else colors.uncheckedColor
 
     Box(
         modifier = modifier
+            .let {
+                if (onCheckedChange != null) {
+                    it.clickableWithoutRipple { onCheckedChange(!checked) }
+                } else {
+                    it
+                }
+            }
             .background(
                 color = BitnagilTheme.colors.white,
                 shape = RoundedCornerShape(6.dp),
@@ -58,5 +71,9 @@ data class BitnagilCheckBoxColor(
 @Preview
 @Composable
 private fun BitnagilCheckBoxPreview() {
-    BitnagilCheckBox(checked = true)
+    var checked by remember { mutableStateOf(false) }
+    BitnagilCheckBox(
+        checked = checked,
+        onCheckedChange = { checked = !checked },
+    )
 }
