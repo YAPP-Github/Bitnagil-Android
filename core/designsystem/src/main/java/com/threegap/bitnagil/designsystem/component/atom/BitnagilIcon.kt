f3f1f14198b596fe2195e122f5b09440dffb2b9d
wjdrjs00 <kimjk3409@gmail.com>
2025-07-29 13:20:42 +0900

    Feat: BitnagilIconButton 컴포넌트 추가

--- core/designsystem/src/main/java/com/threegap/bitnagil/designsystem/component/atom/BitnagilIcon.kt
+++ core/designsystem/src/main/java/com/threegap/bitnagil/designsystem/component/atom/BitnagilIcon.kt
@@ -2,15 +2,21 @@ package com.threegap.bitnagil.designsystem.component.atom
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.threegap.bitnagil.designsystem.BitnagilTheme
 import com.threegap.bitnagil.designsystem.R
+import com.threegap.bitnagil.designsystem.modifier.clickableWithoutRipple
 
 @Composable
 fun BitnagilIcon(
@@ -24,6 +30,31 @@ fun BitnagilIcon(
         colorFilter = tint?.let { ColorFilter.tint(it) },
         modifier = modifier,
     )
+}
+
+@Composable
+fun BitnagilIconButton(
+    @DrawableRes id: Int,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    paddingValues: PaddingValues = PaddingValues(0.dp),
+    tint: Color? = BitnagilTheme.colors.black,
+) {
+    Box(
+        modifier = modifier
+            .clickableWithoutRipple(
+                enabled = enabled,
+                onClick = onClick,
+            )
+            .padding(paddingValues),
+        contentAlignment = Alignment.Center,
+    ) {
+        BitnagilIcon(
+            id = id,
+            tint = tint,
+        )
+    }
 }
 
 @Preview
