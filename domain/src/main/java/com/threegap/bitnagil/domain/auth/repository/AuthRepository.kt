1bbd5eeb8ae97413683c8a5be60897f0cbf9f26e
wjdrjs00 <kimjk3409@gmail.com>
2025-08-09 14:23:55 +0900

    Refactor: 자동 로그인 기능 변경
    - hasTokenUsecase 제거
    - 리프레시 토큰을 기반으로 자동 로그인 시도

--- domain/src/main/java/com/threegap/bitnagil/domain/auth/repository/AuthRepository.kt
+++ domain/src/main/java/com/threegap/bitnagil/domain/auth/repository/AuthRepository.kt
@@ -5,14 +5,12 @@ import com.threegap.bitnagil.domain.auth.model.TermsAgreement
 
 interface AuthRepository {
     suspend fun login(socialAccessToken: String, socialType: String): Result<AuthSession>
-
-    suspend fun hasToken(): Boolean
-
-    suspend fun updateAuthToken(accessToken: String, refreshToken: String): Result<Unit>
-
     suspend fun submitAgreement(termsAgreement: TermsAgreement): Result<Unit>
-
     suspend fun logout(): Result<Unit>
-
     suspend fun withdrawal(): Result<Unit>
+
+    suspend fun reissueToken(refreshToken: String): Result<AuthSession>
+    suspend fun getRefreshToken(): String?
+    suspend fun updateAuthToken(accessToken: String, refreshToken: String): Result<Unit>
+    suspend fun clearAuthToken(): Result<Unit>
 }
