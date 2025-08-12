0c65ddefe6e85ded3d13f401e7d43613fc78fe82
wjdrjs00 <kimjk3409@gmail.com>
2025-08-12 18:06:10 +0900

    Refactor: RoutineSection 컴포넌트 리디자인 반영

--- presentation/src/main/java/com/threegap/bitnagil/presentation/home/component/template/RoutineSection.kt
+++ presentation/src/main/java/com/threegap/bitnagil/presentation/home/component/template/RoutineSection.kt
@@ -1,28 +1,17 @@
 package com.threegap.bitnagil.presentation.home.component.template
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.threegap.bitnagil.designsystem.BitnagilTheme
 import com.threegap.bitnagil.domain.routine.model.RoutineType
 import com.threegap.bitnagil.presentation.home.component.block.RoutineItem
-import com.threegap.bitnagil.presentation.home.component.block.SubRoutinesItem
 import com.threegap.bitnagil.presentation.home.model.RoutineUiModel
 import com.threegap.bitnagil.presentation.home.model.SubRoutineUiModel
 import com.threegap.bitnagil.presentation.home.util.formatExecutionTime
@@ -32,76 +21,31 @@ fun RoutineSection(
     routine: RoutineUiModel,
     onRoutineToggle: (Boolean) -> Unit,
     onSubRoutineToggle: (String, Boolean) -> Unit,
-    onMoreClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Column(
-        modifier = modifier,
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
     ) {
         Text(
-            text = "${routine.executionTime.formatExecutionTime()}부터 시작",
-            style = BitnagilTheme.typography.caption1Regular,
-            color = BitnagilTheme.colors.navy300,
+            text = routine.executionTime.formatExecutionTime(),
+            style = BitnagilTheme.typography.body2Medium,
+            color = BitnagilTheme.colors.coolGray10,
+            modifier = Modifier.defaultMinSize(minWidth = 42.dp),
         )
 
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Row(
-            modifier = Modifier.height(IntrinsicSize.Min),
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .padding(
-                        start = 9.dp,
-                        end = 10.dp,
-                    ),
-            ) {
-                VerticalDivider(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .border(
-                            width = 1.dp,
-                            color = BitnagilTheme.colors.lightBlue300,
-                        )
-                        .align(Alignment.Center),
-                )
-
-                Box(
-                    modifier = Modifier
-                        .padding(top = 26.5.dp)
-                        .size(8.dp)
-                        .background(
-                            color = BitnagilTheme.colors.navy500,
-                            shape = CircleShape,
-                        ),
-                )
-            }
-
-            Column {
-                RoutineItem(
-                    routine = routine,
-                    onRoutineToggle = onRoutineToggle,
-                    onMoreClick = onMoreClick,
-                )
-
-                if (routine.subRoutines.isNotEmpty()) {
-                    SubRoutinesItem(
-                        subRoutines = routine.subRoutines,
-                        onSubRoutineToggle = { subRoutineId, isCompleted ->
-                            onSubRoutineToggle(subRoutineId, isCompleted)
-                        },
-                        modifier = Modifier.padding(top = 10.dp),
-                    )
-                }
-            }
-        }
+        RoutineItem(
+            routine = routine,
+            onRoutineToggle = onRoutineToggle,
+            onSubRoutineToggle = onSubRoutineToggle,
+            modifier = Modifier.fillMaxWidth(),
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-private fun RoutineTemplatePreview() {
+private fun RoutineSectionPreview() {
     RoutineSection(
         routine = RoutineUiModel(
             routineId = "uuid1",
@@ -148,6 +92,5 @@ private fun RoutineTemplatePreview() {
         ),
         onRoutineToggle = {},
         onSubRoutineToggle = { _, _ -> },
-        onMoreClick = {},
     )
 }
