d9c0f71d2016ecc57d9bda54d9ae6d762852bd1c
wjdrjs00 <kimjk3409@gmail.com>
2025-08-21 20:56:20 +0900

    Feat: 더보기 설명 추가

--- presentation/src/main/java/com/threegap/bitnagil/presentation/guide/model/GuideType.kt
+++ presentation/src/main/java/com/threegap/bitnagil/presentation/guide/model/GuideType.kt
@@ -13,6 +13,11 @@ enum class GuideType(
         description = "오늘 느끼는 감정을 선택하면, 그 기분에 맞춰 추천 루틴을 확인할 수 있어요.",
         image = R.drawable.img_register_guide,
     ),
+    HOME_LIST(
+        title = "홈에서 더보기란?",
+        description = "루틴리스트에서 세부 항목(세부루틴, 반복, 기간, 시간)을\n확인하고, 필요 시 수정하거나 삭제할 수 있어요.",
+        image = R.drawable.img_list_guide,
+    ),
     RECOMMEND(
         title = "맞춤 추천 루틴이란?",
         description = "처음 설정한 목표에 맞춰 루틴을 추천받을 수 있으며,\n필요할 때 언제든 수정할 수 있어요.",
