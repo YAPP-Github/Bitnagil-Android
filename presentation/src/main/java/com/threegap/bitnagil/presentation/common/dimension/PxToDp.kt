1b08cab959db25f54ed44aae8cdc6aaead19a8a0
yunsehwan <sh10302@naver.com>
2025-09-09 21:42:11 +0900

    FIX: 기기의 높이가 600.dp를 넘지 못하는 경우, 기존 감정구슬 선택화면이 표시되도록 수정

--- presentation/src/main/java/com/threegap/bitnagil/presentation/common/dimension/PxToDp.kt
+++ presentation/src/main/java/com/threegap/bitnagil/presentation/common/dimension/PxToDp.kt
@@ -5,3 +5,6 @@ import androidx.compose.ui.platform.LocalDensity
 
 @Composable
 fun Float.pxToDp() = with(LocalDensity.current) { this@pxToDp.toDp() }
+
+@Composable
+fun Int.pxToDp() = with(LocalDensity.current) { this@pxToDp.toDp() }
--- presentation/src/main/java/com/threegap/bitnagil/presentation/emotion/EmotionScreen.kt
+++ presentation/src/main/java/com/threegap/bitnagil/presentation/emotion/EmotionScreen.kt
@@ -1,14 +1,20 @@
 package com.threegap.bitnagil.presentation.emotion
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.threegap.bitnagil.presentation.common.dimension.pxToDp
 import com.threegap.bitnagil.presentation.common.flow.collectAsEffect
 import com.threegap.bitnagil.presentation.common.toast.GlobalBitnagilToast
 import com.threegap.bitnagil.presentation.emotion.component.template.EmotionRecommendRoutineScreen
+import com.threegap.bitnagil.presentation.emotion.component.template.SimpleEmotionSelectionScreen
 import com.threegap.bitnagil.presentation.emotion.component.template.SwipeEmotionSelectionScreen
 import com.threegap.bitnagil.presentation.emotion.model.EmotionScreenStep
 import com.threegap.bitnagil.presentation.emotion.model.mvi.EmotionSideEffect
@@ -32,15 +38,27 @@ fun EmotionScreenContainer(
     }
 
     when (state.step) {
-        EmotionScreenStep.Emotion -> SwipeEmotionSelectionScreen(
-            state = state,
-            onClickPreviousButton = navigateToBack,
-            onSelectEmotion = remember {
-                { emotionType ->
-                    viewModel.selectEmotion(emotionType = emotionType, minimumDelay = 1000)
-                }
-            },
-        )
+        EmotionScreenStep.Emotion -> BoxWithConstraints(modifier = Modifier.fillMaxWidth()) {
+            val height = constraints.maxHeight.pxToDp()
+
+            if (height > 600.dp) {
+                SwipeEmotionSelectionScreen(
+                    state = state,
+                    onClickPreviousButton = navigateToBack,
+                    onSelectEmotion = remember {
+                        { emotionType ->
+                            viewModel.selectEmotion(emotionType = emotionType, minimumDelay = 1000)
+                        }
+                    },
+                )
+            } else {
+                SimpleEmotionSelectionScreen(
+                    state = state,
+                    onClickPreviousButton = navigateToBack,
+                    onClickEmotion = viewModel::selectEmotion,
+                )
+            }
+        }
         EmotionScreenStep.RecommendRoutines -> EmotionRecommendRoutineScreen(
             state = state,
             onClickRoutine = viewModel::selectRecommendRoutine,
