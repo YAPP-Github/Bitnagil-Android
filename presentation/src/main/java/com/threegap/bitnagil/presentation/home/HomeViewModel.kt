1b707befd23e7b0e41da2e9062d71ad9d67d2e71
wjdrjs00 <kimjk3409@gmail.com>
2025-10-28 16:26:08 +0900

    chore: import 문 제거

--- presentation/src/main/java/com/threegap/bitnagil/presentation/home/HomeViewModel.kt
+++ presentation/src/main/java/com/threegap/bitnagil/presentation/home/HomeViewModel.kt
@@ -22,7 +22,6 @@ import com.threegap.bitnagil.presentation.home.model.toUiModel
 import com.threegap.bitnagil.presentation.home.util.getCurrentWeekDays
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
