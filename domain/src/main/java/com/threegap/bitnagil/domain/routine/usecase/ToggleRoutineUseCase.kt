0d93d5f0a219d161acd750c18b359161d1328df9
wjdrjs00 <kimjk3409@gmail.com>
2025-11-08 18:50:36 +0900

    Feat: 루틴 토글 usecase 로직 추가
    - RoutineToggleState 정의
    - 기존 viewmodel에서 비즈니스 로직 domain으로 변경

---
+++ domain/src/main/java/com/threegap/bitnagil/domain/routine/usecase/ToggleRoutineUseCase.kt
@@ -0,0 +1,57 @@
+package com.threegap.bitnagil.domain.routine.usecase
+
+import com.threegap.bitnagil.domain.routine.model.RoutineToggleState
+import javax.inject.Inject
+
+class ToggleRoutineUseCase @Inject constructor() {
+
+    /**
+     * 메인 루틴을 토글합니다.
+     * 메인 루틴이 토글되면 모든 서브 루틴도 같은 상태로 변경됩니다.
+     *
+     * @param isCompleted 현재 메인 루틴 완료 상태
+     * @param subRoutineStates 현재 서브 루틴 완료 상태 리스트
+     * @return 토글된 루틴 상태
+     */
+    fun toggleMainRoutine(
+        isCompleted: Boolean,
+        subRoutineStates: List<Boolean>,
+    ): RoutineToggleState {
+        val newIsCompleted = !isCompleted
+        val newSubRoutineStates = subRoutineStates.map { newIsCompleted }
+
+        return RoutineToggleState(
+            isCompleted = newIsCompleted,
+            subRoutinesIsCompleted = newSubRoutineStates,
+        )
+    }
+
+    /**
+     * 특정 서브 루틴을 토글합니다.
+     * 모든 서브 루틴이 완료되면 메인 루틴도 자동으로 완료됩니다.
+     *
+     * @param index 토글할 서브 루틴의 인덱스
+     * @param subRoutineStates 현재 서브 루틴 완료 상태 리스트
+     * @return 토글된 루틴 상태, 잘못된 인덱스인 경우 null
+     */
+    fun toggleSubRoutine(
+        index: Int,
+        subRoutineStates: List<Boolean>,
+    ): RoutineToggleState? {
+        if (index !in subRoutineStates.indices) {
+            return null
+        }
+
+        val newState = !subRoutineStates[index]
+        val newSubRoutineStates = subRoutineStates.toMutableList().apply {
+            this[index] = newState
+        }
+
+        val allCompleted = newSubRoutineStates.all { it }
+
+        return RoutineToggleState(
+            isCompleted = allCompleted,
+            subRoutinesIsCompleted = newSubRoutineStates,
+        )
+    }
+}
