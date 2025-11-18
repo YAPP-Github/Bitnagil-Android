0ae10117effcf067ff4c5198d388e393b7a481ca
wjdrjs00 <kimjk3409@gmail.com>
2025-11-18 14:13:58 +0900

    Feat: 현재 위치를 가져오는 기능 추가

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation(libs.lottie.compose)
     implementation(libs.bundles.coil)
     implementation(libs.accompanist.permissions)
+    implementation(libs.play.services.location)
 
     testImplementation(libs.junit)
     testImplementation(libs.kotlin.coroutines.test)
