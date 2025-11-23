0cfd13e38222f23b07dcba05699652e3d5b70454
yunsehwan <sh10302@naver.com>
2025-11-23 12:15:47 +0900

    Feat: 제보 상세조회 화면 UI 구현

---
+++ presentation/src/main/java/com/threegap/bitnagil/presentation/reportdetail/model/navarg/ReportDetailScreenArg.kt
@@ -0,0 +1,8 @@
+package com.threegap.bitnagil.presentation.reportdetail.model.navarg
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ReportDetailScreenArg(
+    val reportId: Int,
+)
