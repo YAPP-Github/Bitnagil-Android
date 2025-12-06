ef9a971c1c42c92baaaa71ed5b8947f4a3a82fce
yunsehwan <sh10302@naver.com>
2025-12-06 20:32:12 +0900

    Fix: MviViewModel 관련 코드 제거 및 SettingSideEffect가 MviSideEffect를 상속하고 있던 문제 수정

--- presentation/src/main/java/com/threegap/bitnagil/presentation/setting/model/mvi/SettingSideEffect.kt
+++ presentation/src/main/java/com/threegap/bitnagil/presentation/setting/model/mvi/SettingSideEffect.kt
@@ -1,8 +1,6 @@
 package com.threegap.bitnagil.presentation.setting.model.mvi
 
-import com.threegap.bitnagil.presentation.common.mviviewmodel.MviSideEffect
-
-sealed class SettingSideEffect : MviSideEffect {
+sealed class SettingSideEffect {
     data object NavigateToLogin : SettingSideEffect()
     data object NavigateToWithdrawal : SettingSideEffect()
 }
