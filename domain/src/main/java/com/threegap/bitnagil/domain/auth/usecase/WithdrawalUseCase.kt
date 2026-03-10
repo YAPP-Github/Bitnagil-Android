35bf09269355aa4f8b44cc0a7a16b3f47b40fbd2
wjdrjs00 <kimjk3409@gmail.com>
2026-03-11 02:51:51 +0900

    Feat: 로그아웃 및 회원탈퇴 성공 시 유저 정보 캐시를 초기화하도록 수정

--- domain/src/main/java/com/threegap/bitnagil/domain/auth/usecase/WithdrawalUseCase.kt
+++ domain/src/main/java/com/threegap/bitnagil/domain/auth/usecase/WithdrawalUseCase.kt
@@ -1,10 +1,15 @@
 package com.threegap.bitnagil.domain.auth.usecase
 
 import com.threegap.bitnagil.domain.auth.repository.AuthRepository
+import com.threegap.bitnagil.domain.user.repository.UserRepository
 import javax.inject.Inject
 
 class WithdrawalUseCase @Inject constructor(
     private val authRepository: AuthRepository,
+    private val userRepository: UserRepository,
 ) {
-    suspend operator fun invoke(reason: String): Result<Unit> = authRepository.withdrawal(reason)
+    suspend operator fun invoke(reason: String): Result<Unit> =
+        authRepository.withdrawal(reason).onSuccess {
+            userRepository.clearCache()
+        }
 }
