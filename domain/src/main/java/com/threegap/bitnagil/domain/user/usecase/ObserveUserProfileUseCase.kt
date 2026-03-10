1b40a14b2e48a447a0812891f6073fb5260f1a3e
wjdrjs00 <kimjk3409@gmail.com>
2026-03-11 02:51:32 +0900

    Refactor: FetchUserProfileUseCase를 ObserveUserProfileUseCase(Flow)로 변경

--- domain/src/main/java/com/threegap/bitnagil/domain/user/usecase/ObserveUserProfileUseCase.kt
+++ domain/src/main/java/com/threegap/bitnagil/domain/user/usecase/ObserveUserProfileUseCase.kt
@@ -2,11 +2,11 @@ package com.threegap.bitnagil.domain.user.usecase
 
 import com.threegap.bitnagil.domain.user.model.UserProfile
 import com.threegap.bitnagil.domain.user.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class FetchUserProfileUseCase @Inject constructor(
+class ObserveUserProfileUseCase @Inject constructor(
     private val userRepository: UserRepository,
 ) {
-    suspend operator fun invoke(): Result<UserProfile> =
-        userRepository.fetchUserProfile()
+    operator fun invoke(): Flow<Result<UserProfile>> = userRepository.observeUserProfile()
 }
