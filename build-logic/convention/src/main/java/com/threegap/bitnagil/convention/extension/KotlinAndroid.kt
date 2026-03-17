f514780cc412c67e44f3d0e5ab96f96e7cf9437c
wjdrjs00 <kimjk3409@gmail.com>
2026-03-17 20:12:49 +0900

    Chore: OkHttp 및 Retrofit 버전 업데이트

--- build-logic/convention/src/main/java/com/threegap/bitnagil/convention/extension/KotlinAndroid.kt
+++ build-logic/convention/src/main/java/com/threegap/bitnagil/convention/extension/KotlinAndroid.kt
@@ -44,6 +44,7 @@ internal fun Project.configureKotlinAndroid(commonExtension: CommonExtension<*, 
             resources {
                 excludes.add("META-INF/AL2.0")
                 excludes.add("META-INF/LGPL2.1")
+                excludes.add("META-INF/versions/9/OSGI-INF/MANIFEST.MF")
             }
         }
 
