a0ce38bca6aa73983dc4cab7b04382d2757e5f68
wjdrjs00 <kimjk3409@gmail.com>
2026-03-27 02:22:31 +0900

    Refactor: HomeViewModel 단순화

--- presentation/src/main/java/com/threegap/bitnagil/presentation/screen/home/contract/HomeState.kt
+++ presentation/src/main/java/com/threegap/bitnagil/presentation/screen/home/contract/HomeState.kt
@@ -7,22 +7,17 @@ import com.threegap.bitnagil.presentation.screen.home.util.getCurrentWeekDays
 import java.time.LocalDate
 
 data class HomeState(
-    val loadingCount: Int,
     val userNickname: String,
     val dailyEmotion: DailyEmotionUiModel,
     val selectedDate: LocalDate,
     val currentWeeks: List<LocalDate>,
     val routineSchedule: RoutineScheduleUiModel,
 ) {
-    val isLoading: Boolean
-        get() = loadingCount > 0
-
     val selectedDateRoutines: List<RoutineUiModel>
         get() = routineSchedule.dailyRoutines[selectedDate.toString()]?.routines ?: emptyList()
 
     companion object {
         val INIT = HomeState(
-            loadingCount = 0,
             userNickname = "",
             dailyEmotion = DailyEmotionUiModel.INIT,
             selectedDate = LocalDate.now(),
