22924533e9573694e6e98ea121a7bf1ff9fcb59d
wjdrjs00 <kimjk3409@gmail.com>
2026-04-01 17:56:28 +0900

    Feat: Coil 라이브러리 커스텀 ImageLoader 설정

--- app/src/main/java/com/threegap/bitnagil/BitnagilApplication.kt
+++ app/src/main/java/com/threegap/bitnagil/BitnagilApplication.kt
@@ -1,14 +1,52 @@
 package com.threegap.bitnagil
 
 import android.app.Application
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.disk.DiskCache
+import coil3.memory.MemoryCache
+import coil3.network.okhttp.OkHttpNetworkFetcherFactory
+import coil3.request.CachePolicy
+import coil3.request.crossfade
+import coil3.util.DebugLogger
 import com.kakao.sdk.common.KakaoSdk
+import com.threegap.bitnagil.di.core.CoilEntryPoint
+import dagger.hilt.EntryPoints
 import dagger.hilt.android.HiltAndroidApp
+import okio.Path.Companion.toOkioPath
 
 @HiltAndroidApp
-class BitnagilApplication : Application() {
+class BitnagilApplication : Application(), SingletonImageLoader.Factory {
+
     override fun onCreate() {
         super.onCreate()
         initKakaoSdk()
+    }
+
+    override fun newImageLoader(context: PlatformContext): ImageLoader {
+        val okHttpClient = EntryPoints
+            .get(this, CoilEntryPoint::class.java)
+            .noneAuthOkHttpClient()
+
+        return ImageLoader.Builder(context)
+            .components { add(OkHttpNetworkFetcherFactory(callFactory = { okHttpClient })) }
+            .memoryCache {
+                MemoryCache.Builder()
+                    .maxSizePercent(context, percent = 0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(cacheDir.resolve("image_cache").toOkioPath())
+                    .maxSizeBytes(50L * 1024 * 1024)
+                    .build()
+            }
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .crossfade(true)
+            .logger(if (BuildConfig.DEBUG) DebugLogger() else null)
+            .build()
     }
 
     private fun initKakaoSdk() {
