84edfcf489e1764ad011a971d2671cc0a52ffe14
wjdrjs00 <kimjk3409@gmail.com>
2026-08-29 18:48:44 +0900

    Feat: Meta 최초 루틴 달성 이벤트 추가
    표준 이벤트 ACHIEVED_LEVEL 로 기록한다.
    재사용 가능성이 높은 이벤트라 나중에 다른 마일스톤과 구분할 수 있도록 EVENT_PARAM_LEVEL 을 함께 보낸다.

--- core/analytics/src/main/java/com/threegap/bitnagil/analytics/AnalyticsLogger.kt
+++ core/analytics/src/main/java/com/threegap/bitnagil/analytics/AnalyticsLogger.kt
@@ -5,4 +5,9 @@ interface AnalyticsLogger {
      * 온보딩 완료를 기록한다. 추천 루틴을 등록했든 건너뛰었든 동일하게 기록한다.
      */
     fun logOnBoardingCompleted()
+
+    /**
+     * 생애 최초 루틴 완료를 기록한다. 앱 설치 기준으로 1회만 호출된다.
+     */
+    fun logFirstRoutineCompleted()
 }
--- core/analytics/src/main/java/com/threegap/bitnagil/analytics/meta/MetaAnalyticsLogger.kt
+++ core/analytics/src/main/java/com/threegap/bitnagil/analytics/meta/MetaAnalyticsLogger.kt
@@ -1,6 +1,7 @@
 package com.threegap.bitnagil.analytics.meta
 
 import android.content.Context
+import android.os.Bundle
 import com.facebook.appevents.AppEventsConstants
 import com.facebook.appevents.AppEventsLogger
 import com.threegap.bitnagil.analytics.AnalyticsLogger
@@ -11,5 +12,17 @@ class MetaAnalyticsLogger(private val context: Context) : AnalyticsLogger {
 
     override fun logOnBoardingCompleted() {
         logger.logEvent(AppEventsConstants.EVENT_NAME_COMPLETED_TUTORIAL)
+    }
+
+    override fun logFirstRoutineCompleted() {
+        val parameters = Bundle().apply {
+            putString(AppEventsConstants.EVENT_PARAM_LEVEL, LEVEL_FIRST_ROUTINE_COMPLETED)
+        }
+
+        logger.logEvent(AppEventsConstants.EVENT_NAME_ACHIEVED_LEVEL, parameters)
+    }
+
+    private companion object {
+        const val LEVEL_FIRST_ROUTINE_COMPLETED = "first_routine_completed"
     }
 }
